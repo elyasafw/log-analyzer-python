5a34351bac7f7825d4b96e695b6c83cf22919324
elyasaf <elyasafw1997@gmail.com>
2026-02-11 14:04:31 +0200

    add filter_by_size funciton

--- log_analyzer/reader.py
+++ log_analyzer/reader.py
@@ -10,3 +10,13 @@ def load_log_file(file_path):
 def external_ip_extraction(list_logs):
     external_ip = [ip[1] for ip in list_logs if not ip[1].startswith(('192.168', '10'))]
     return external_ip
+
+
+def sensitive_port_filtering(list_logs):
+    sensitive_ports = [port for port in list_logs if port[3].startswith(('22', '23', '3389'))]
+    return sensitive_ports
+
+
+def filter_by_size(list_logs):
+    filter_size = [size for size in list_logs if int(size[-1]) > 5000]
+    return filter_size

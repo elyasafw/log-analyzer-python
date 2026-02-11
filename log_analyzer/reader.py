087e2beabbaf7721c2afd2617ca33676b9458da6
elyasaf <elyasafw1997@gmail.com>
2026-02-11 12:08:27 +0200

    add external_ip_extraction funciton

--- log_analyzer/reader.py
+++ log_analyzer/reader.py
@@ -5,3 +5,8 @@ def load_log_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         list_logs = [line for line in csv.reader(file)]
     return list_logs
+
+
+def external_ip_extraction(list_logs):
+    external_ip = [ip[1] for ip in list_logs if not ip[1].startswith(('192.168', '10'))]
+    return external_ip

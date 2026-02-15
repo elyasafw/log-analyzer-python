eb53cc9aba9cc335a19d133ccd0dbfd2d0c5a69f
elyasaf <elyasafw1997@gmail.com>
2026-02-15 15:30:41 +0200

    add menu

--- log_analyzer/reporter.py
+++ log_analyzer/reporter.py
@@ -1,10 +1,13 @@
-from reader import load_log_func, load_log_gen
+from reader import load_log_func, load_log_gen, external_ip_extraction, filter_by_size
 from checks import check_row_suspicions, process_all_logs_gen, suspicions_checks
-from analyzer import ip_suspicions
+from analyzer import ip_suspicions, port_to_protocol, req_count_by_ip
+from pathlib import Path
+
+path = Path(r'./network_traffic.log')
 
 
 def extract_hours():
-    logs = load_log_func(r'./network_traffic.log')
+    logs = load_log_func(path)
     
     hours_list = list(map(
         lambda log_line: int(log_line[0][11:13]),
@@ -15,7 +18,7 @@ def extract_hours():
 
 
 def package_size_conversion():
-    logs = load_log_func(r'./network_traffic.log')
+    logs = load_log_func(path)
 
     package_size = list(map(
         lambda package: float(int(package[-1]) // 1024),
@@ -26,7 +29,7 @@ def package_size_conversion():
 
 
 def filter_by_port():
-    logs = load_log_func(r'./network_traffic.log')
+    logs = load_log_func(path)
 
     sensitive_lines = list(filter(
         lambda line: suspicions_checks["PORT_SENSITIVE"](line),
@@ -37,7 +40,7 @@ def filter_by_port():
 
 
 def filter_night_activity():
-    logs = load_log_func(r'./network_traffic.log')
+    logs = load_log_func(path)
 
     night_logs = list(filter(
         lambda log: suspicions_checks["ACTIVITY_NIGHT"](log),
@@ -56,7 +59,7 @@ def sum_suspicious_rows(line_details):
     return sum(1 for log in line_details if len(log[1]) > 0)
 
 
-lines = load_log_gen(r'./network_traffic.log') # generator
+lines = load_log_gen(path) # generator
 suspicious = process_all_logs_gen(lines) # generator
 detailed = suspicions_with_details_gen(suspicious) # generator
 
@@ -97,63 +100,96 @@ def log_analyze(file_path):
     return suspicions_dict
 
 
-
-# def generate_report(suspicious_dict):
-#     global total_lines_read, total_suspicious_lines, suspicious_counts
-    
-#     report = "=======================================\n"
-#     report += "       Suspicious Traffic Report\n"
-#     report += "=======================================\n\n"
-    
-#     report += "General Statistics:\n"
-#     report += f"- Lines read: {total_lines_read}\n"
-#     report += f"- Suspicious lines: {total_suspicious_lines}\n"
-    
-#     for name, count in suspicious_counts.items():
-#         report += f"- {name}: {count}\n"
-        
-#     report += "\nHigh Risk IPs (3+ threats):\n"
-    
-#     for ip, threats in suspicious_dict.items():
-#         if len(threats) >= 3:
-#             report += f"- {ip}: {', '.join(threats)}\n"
-            
-#     report += "\nOther Suspicious IPs:\n"
-    
-#     for ip, threats in suspicious_dict.items():
-#         if len(threats) < 3:
-#             report += f"- {ip}: {', '.join(threats)}\n"
-            
-#     return report
-
-
 def generate_report(suspicious_dict):
     global total_lines_read, total_suspicious_lines, suspicious_counts
 
     stats_section = [
-        "\n=======================================",
-        "        SUSPICIOUS TRAFFIC REPORT",
-        "=======================================",
-        "\nGeneral Statistics:",
-        f"- Total lines read: {total_lines_read:,}",
-        f"- Total suspicious lines: {total_suspicious_lines:,}"
+        "\n\n\t=======================================",
+        "        \tSUSPICIOUS TRAFFIC REPORT",
+        "\t=======================================",
+        "\n\tGeneral Statistics:",
+        f"\t- Total lines read: {total_lines_read:,}",
+        f"\t- Total suspicious lines: {total_suspicious_lines:,}"
     ]
     
     for name, count in suspicious_counts.items():
-        stats_section.append(f"- {name}: {count:,}")
+        stats_section.append(f"\t- {name}: {count:,}")
 
     high_risk = []
     other_suspicious = []
     
     for ip, threats in suspicious_dict.items():
-        entry = f"- {ip}: {', '.join(threats)}"
+        entry = f"\t- {ip}: {', '.join(threats)}"
         if len(threats) >= 3:
             high_risk.append(entry)
         else:
             other_suspicious.append(entry)
 
     report_body = stats_section + \
-                  ["\n--- High Risk IPs (3+ threats):\n"] + high_risk + \
-                  ["\n--- Other Suspicious IPs:\n"] + other_suspicious
+                  ["\n\t--- High Risk IPs (3+ threats):\n"] + high_risk + \
+                  ["\n\t--- Other Suspicious IPs:\n"] + other_suspicious
 
     return "\n".join(report_body)
+
+
+def print_log():
+    print("\n\n\t--- Log report ---\n")
+    count = 0
+    for i in sorted(load_log_gen(path), key=lambda x: x[0]):
+        count += 1
+        print(f"\t-  {" | ".join(i)}")
+    print(f"\n\tTotal log lines:: {count}")
+
+
+def print_external_ips():
+    print("\n\n\t--- List of external IP addresses ---\n")
+    count = 0
+    for i in sorted(external_ip_extraction(load_log_gen(path))):
+        count += 1
+        print(f"\t-  {i}")
+    print(f"\n\tTotal external IP addresses found: {count}")
+
+
+def print_suspicious():
+    print("\n\n\t--- List of suspicious IP addresses ---\n")
+    count = 0
+    for k, v in sorted(ip_suspicions(load_log_func(path)).items(), key=lambda x: len(x[1])):
+        count += 1
+        print(f"\t-  {k} :  {" | ".join(v)}")
+    print(f"\n\tTotal suspicious IP addresses found: {count}")
+
+
+def print_ports():
+    print("\n\n\t--- Port list ---\n")
+    count = 0
+    for k, v in port_to_protocol(load_log_gen(path)).items():
+        count += 1
+        print(f"\t-  {k} :  {v}")
+    print(f"\n\tTotal ports found: {count}")
+
+
+def print_night_traffic():
+    print("\n\n\t--- Traffic list at: 12 pm to 6 am ---\n")
+    count = 0
+    for i in sorted(filter_night_activity(), key=lambda x: x[0]):
+        count += 1
+        print(f"\t-  {" | ".join(i)}")
+    print(f"\n\tTotal night traffic found: {count}")
+
+
+def print_req_count():
+    print("\n\n\t--- List of total requests by IP ---\n")
+    count = 0
+    for k, v in sorted(req_count_by_ip(load_log_gen(path)).items(), key=lambda x: x[1]):
+        count += 1
+        print(f"\t-  {k} :  {v}")
+    print(f"\n\tTotal IP addresses: {count}")
+
+
+def print_by_packet():
+    print("\n\n\t--- Logs with a package of 5 MB or more ---\n")
+    count = 0
+    for i in sorted(filter_by_size(load_log_gen(path)), key=lambda x: int(x[5])):
+        count += 1
+        print(f"\t-  {" | ".join(i)}")
+    print(f"\n\tTotal logs found:: {count}")
